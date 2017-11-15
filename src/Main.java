24395144fa26ea8137072f1a8c9740c3aed8d0eb
minchea <33686509+minchea@users.noreply.github.com>
2017-11-15 20:32:50 +0900

    clear

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,13 @@ public class Main {
 		int n = in.nextInt();
 		Stack stack = new Stack();
 		for(int i = 0; i < n; i++){
-			boolean result = stack.push(i);
+			boolean result = stack.push(in.nextInt());
 			if(result == false){
 				System.out.println("stack is full!!!!");
 				break;
 			}
 		}
+		
 		
 		for(int i = 0; i < n; i++) System.out.println(stack.pop());
 	}
